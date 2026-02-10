d77417d61220cdac06cb7136e3d5843e41fbeb75
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-02-10 17:36:38 +0500

    mini Guess Game

--- game.py
+++ game.py
@@ -7,8 +7,8 @@ print("Guess an Number between (1-10)\n")
 guess_num :int =int(input("Enter A Number:"))
 
 if guess_num == random_number:
-    print("Yahooo Mn Jeet Gya" ,random_number)
+    print(f"Yahooo Mn Jeet Gya {random_number}")
     # break
 else:
-    print("Try Again" , random_number)
+    print(f"Try Again , {random_number}")
 
