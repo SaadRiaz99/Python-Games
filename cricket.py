a50f8aea79168a5c327bf06fe6b6c7ffdc8e1e73
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-02-10 21:03:05 +0500

    Add Score Card And Runrate System

--- cricket.py
+++ cricket.py
@@ -5,16 +5,28 @@ import random as rand
 print("============Mini-Cricket-match================\n")
 print("Rules: Choose a number 1-6. If your number matches computer's number, you're OUT!")
 
-
+while True:
+    try:
+        over = int(input("Enter how many overs you want to play: "))
+        if over > 0:
+            break
+    except:
+        print("Enter numbers only!")
 user_score = 0
 computer_score = 0
-turns = 6
+turns = 6 * over
 
 
 print("\nYour Batting Turn:")
 
 for i in range(turns):
-    user = int(input(f"Bowl {i+1}: Enter The Number(1-6)"))
+    while True:
+        try:
+            user = int(input(f"Bowl {i+1}: Enter The Number(1-6)"))
+            if 1 <= user <=6:
+                break
+        except:
+            print("Invalid input!")
     comput = rand.randint(1,6)
     print(f"Computer bowled: {comput}")
 
@@ -24,7 +36,16 @@ for i in range(turns):
         break
     else:
         user_score += user
+    print(f"Run Scored {user_score}")
+    if(i+1) % 6  == 0 or i == [turns-1]:
+        over_played = (i+1)//6 + ((i+1) % 6) / 6
+        run_rate = user_score / over_played
+        print(f"\n End of Over {(i+1)//6}")  
+
         print(f"Run Scored {user_score}")
+        print(f"Run Rate: {round(run_rate,2)}\n")
+
+
 print(f"Your total score: {user_score}")        
 
 
@@ -41,6 +62,15 @@ for i in range(turns):
         break
     else:
         computer_score += computer
+    print(f"Run Scored {computer_score }")
+
+    if(i+1) % 6  == 0 or i == [turns-1]:
+        over_played = (i+1)//6 + ((i+1) % 6) / 6
+        run_rate = user_score / overs_played
+        print(f"\n End of Over {(i+1)//6}")  
+
+        print(f"Run Scored {computer_score}")
+        print(f"Run Rate: {round(run_rate,2)}\n")
         print(f"Run Scored {computer_score }")
 print(f"Your total score: {computer_score}")  
 
