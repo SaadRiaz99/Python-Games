882d6ac4b4a66b9c5b6e21e9d70f30813b5f0042
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-02-10 17:49:41 +0500

    Add User batting Program

--- cricket.py
+++ cricket.py
@@ -1,4 +1,4 @@
-import random
+import random as rand
 
 
 
@@ -9,3 +9,23 @@ print("Rules: Choose a number 1-6. If your number matches computer's number, you
 user_score = 0
 computer_score = 0
 turns = 6
+
+
+print("\nYour Batting Turn:")
+
+for i in range(turns):
+    user = int(input(f"Bowl {i+1}: Enter The Number(1-6)"))
+    comput = rand.randint(1,6)
+    print(f"Computer bowled: {comput}")
+
+
+    if user == comput:
+        print("You are Out!")
+        break
+    else:
+        user_score += user
+        print(f"Run Scored {user_score}")
+print(f"Your total score: {user_score}")        
+
+
+print("\nComputer Batting Turn:")
