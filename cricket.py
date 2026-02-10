3b7239aac284c14db80cc0f8e3b2c862825aa32c
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-02-10 17:39:33 +0500

    Add Cricket Game

---
+++ cricket.py
@@ -0,0 +1,11 @@
+import random
+
+
+
+print("============Mini-Cricket-match================\n")
+print("Rules: Choose a number 1-6. If your number matches computer's number, you're OUT!")
+
+
+user_score = 0
+computer_score = 0
+turns = 6
